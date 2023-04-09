d52605b90fcc103b78b3904a2d387898afa81756
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-04-09 12:45:41 -0300

    feat: ensure AccppuntMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,24 @@
+import { AddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import { AddAccountModel } from '../../../../domain/use-cases/add-account'
+import { AccountModel } from '../../../../domain/models/account'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add(accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+
+    const result = await accountCollection.insertOne(accountData)
+
+    const account = await accountCollection.findOne({ _id: result.insertedId })
+    if (!account) throw Error('Account not found')
+
+    const { _id, ...accountWithoutId } = account
+
+    return {
+      id: _id.toHexString(),
+      name: accountWithoutId.name,
+      email: accountWithoutId.email,
+      password: accountWithoutId.password,
+    }
+  }
+}
---
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,21 @@
+import { Collection, MongoClient } from 'mongodb'
+
+export const MongoHelper = {
+  client: null as unknown as MongoClient,
+
+  async connect(uri: any): Promise<void> {
+    // if (!process.env.MONGO_URI) throw new Error('MongoDB URI not found')
+
+    this.client = await MongoClient.connect(
+      'mongodb://localhost:27017/clean-node-api'
+    )
+  },
+
+  async disconnect(): Promise<void> {
+    await this.client.close()
+  },
+
+  getCollection(name: string): Collection {
+    return this.client.db().collection(name)
+  },
+}
