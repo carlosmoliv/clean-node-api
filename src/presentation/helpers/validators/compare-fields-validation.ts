cbb37c5fd612094f00cdc0af67d079855f1cda3a
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-04-12 17:09:13 -0300

    test: add tests to CompareFieldsValidation

--- src/presentation/helpers/validators/compare-fields-validation.ts
+++ src/presentation/helpers/validators/compare-fields-validation.ts
@@ -1,4 +1,4 @@
-import { MissingParamError } from '../../errors'
+import { InvalidParamError } from '../../errors'
 import { Validation } from './validation'
 
 export class CompareFieldsValidation implements Validation {
@@ -9,6 +9,6 @@ export class CompareFieldsValidation implements Validation {
 
   validate(input: any): Error {
     if (input[this.fieldName] !== input[this.fieldToCompareName])
-      return new MissingParamError(this.fieldToCompareName)
+      return new InvalidParamError(this.fieldToCompareName)
   }
 }
