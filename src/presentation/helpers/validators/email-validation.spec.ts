2079189268b8f170309113eea393068141a0da3f
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-04-12 18:13:44 -0300

    refactor: move HttpHelper to a folder

--- src/presentation/helpers/validators/email-validation.spec.ts
+++ src/presentation/helpers/validators/email-validation.spec.ts
@@ -1,6 +1,6 @@
 import { InvalidParamError, ServerError } from '../../errors'
 import { EmailValidator } from '../../protocols/email-validator'
-import { serverError } from '../http-helper'
+import { serverError } from '../http/http-helper'
 import { EmailValidation } from './email-validation'
 
 const makeEmailValidator = (): EmailValidator => {
