81f309b661612c6af4d5e188acf351797b22b69b
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-04-14 20:28:16 -0300

    refactor: move bcrypt-adapter to a folder

--- src/infra/cryptography/bcrypt-adapter/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter/bcrypt-adapter.spec.ts
@@ -51,6 +51,18 @@ describe('Bcrypt Adapter', () => {
     expect(isValid).toBe(true)
   })
 
+  // it('should return false when compare fails', async () => {
+  //   const sut = makeSut()
+
+  //   jest
+  //     .spyOn(bcrypt, 'compare')
+  //     .mockReturnValueOnce(new Promise((resolve) => resolve(false)))
+
+  //   const isValid = await sut.compare('any_value', 'any_hash')
+
+  //   expect(isValid).toBe(false)
+  // })
+
   // it('should throw an error if bcrypt also throws', async () => {
   //   const sut = makeSut()
 
