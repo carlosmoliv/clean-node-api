1a741bc1dec67b54a565fd0526cd57444751045d
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-04-15 11:32:16 -0300

    test: login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -52,5 +52,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    it('should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'johndoe@mail.com',
+          password: '1234',
+        })
+        .expect(401)
+    })
   })
 })
