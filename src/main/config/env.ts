3b850c21063d990943695b7b5317be2baf9f71eb
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-04-15 11:21:24 -0300

    feat: add login factory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUri: process.env.MONGO_URI || 'mongodb://localhost:27017/clean-node-api',
   port: process.env.PORT || 5050,
+  jwtSecret: process.env.JWT_SECRET || 'ew87#D',
 }
---
+++ src/main/factories/login/login-factory.ts
@@ -0,0 +1,34 @@
+import env from '../../config/env'
+import { LogControllerDecorator } from '../../decorators/log'
+import { Controller } from '../../../presentation/protocols'
+import { LogMongoRepository } from '../../../infra/db/mongodb/log/log-mongo-repository'
+import { makeLoginValidation } from './login-validation-factory'
+import { DbAuthentication } from '../../../data/use-cases/authentication/db-authentication'
+import { LoginController } from '../../../presentation/controllers/login/login-controller'
+import { AccountMongoRepository } from '../../../infra/db/mongodb/account/account-mongo-repositpory'
+import { BcryptAdapter } from '../../../infra/cryptography/bcrypt-adapter/bcrypt-adapter'
+import { JwtAdapter } from '../../../infra/cryptography/jwt-adapter/jwt-adapter'
+
+export const makeLoginCOntroller = (): Controller => {
+  const salt = 12
+
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const jwtAdapter = new JwtAdapter(env.jwtSecret)
+  const accountMongoRepository = new AccountMongoRepository()
+
+  const dbAuthentication = new DbAuthentication(
+    accountMongoRepository,
+    bcryptAdapter,
+    jwtAdapter,
+    accountMongoRepository
+  )
+
+  const loginController = new LoginController(
+    dbAuthentication,
+    makeLoginValidation()
+  )
+
+  const logMongoRepository = new LogMongoRepository()
+
+  return new LogControllerDecorator(loginController, logMongoRepository)
+}
