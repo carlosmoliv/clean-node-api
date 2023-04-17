69185c3f496f5c59af2bfc42e68c49dd35512c09
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-04-17 17:59:58 -0300

    feat: esnure AddSurveyController calls AddSurveyUseCase with correct values

--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -1,21 +1,31 @@
-import { badRequest, serverError } from '../../../helpers/http/http-helper'
+import { badRequest } from '../../../helpers/http/http-helper'
 import {
   Controller,
   HttpRequest,
   HttpResponse,
   Validation,
+  AddSurvey,
 } from './add-survey-controller-protocols'
 
 export class AddSurveyController implements Controller {
-  constructor(private readonly validation: Validation) {}
+  constructor(
+    private readonly validation: Validation,
+    private readonly addSurvey: AddSurvey
+  ) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    const { question, answers } = httpRequest.body
     const error = this.validation.validate(httpRequest.body)
 
     if (error) {
       return badRequest(error)
     }
 
-    return new Promise((resolve) => resolve(null))
+    await this.addSurvey.add({
+      question,
+      answers,
+    })
+
+    return null
   }
 }
