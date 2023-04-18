496f2fd01d4be9ec014b272ee0d1780a0c178ce7
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-04-17 22:39:30 -0300

    feat: add AddSurveyRoute composition

---
+++ src/main/routes/survey-routes.test.ts
@@ -0,0 +1,41 @@
+import request from 'supertest'
+import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import { Collection } from 'mongodb'
+
+let surveyCollection: Collection
+
+describe('Login Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect('mongodb://localhost:27017/clean-node-api')
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+  })
+
+  describe('POST /surveys', () => {
+    it('should return 204 on add survey', async () => {
+      await request(app)
+        .post('/api/surveys')
+        .send({
+          question: 'Question',
+          answers: [
+            {
+              image: 'http://image-name.com',
+              answer: 'Answer 1',
+            },
+            {
+              answer: 'Answer 2',
+            },
+          ],
+        })
+        .expect(204)
+    })
+  })
+})
