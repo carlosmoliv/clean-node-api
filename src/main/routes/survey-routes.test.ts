b9dddd47443bf1ea4930c7b033849156cd500fbe
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-04-21 13:09:00 -0300

    test: ensure add survey route returns 403 if access token is not provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -20,7 +20,7 @@ describe('Login Routes', () => {
   })
 
   describe('POST /surveys', () => {
-    it('should return 204 on add survey', async () => {
+    it('should return 403 on add survey without access token', async () => {
       await request(app)
         .post('/api/surveys')
         .send({
@@ -35,7 +35,7 @@ describe('Login Routes', () => {
             },
           ],
         })
-        .expect(204)
+        .expect(403)
     })
   })
 })
