56b7d7d338fbed6948524783528217104d42356a
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-04-21 13:17:21 -0300

    test: ensure add survey route returns 204 if access token is valid

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -2,8 +2,11 @@ import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import { Collection } from 'mongodb'
+import { sign } from 'jsonwebtoken'
+import env from '../config/env'
 
 let surveyCollection: Collection
+let accountCollection: Collection
 
 describe('Login Routes', () => {
   beforeAll(async () => {
@@ -17,6 +20,9 @@ describe('Login Routes', () => {
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.deleteMany({})
+
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
   })
 
   describe('POST /surveys', () => {
@@ -36,6 +42,46 @@ describe('Login Routes', () => {
           ],
         })
         .expect(403)
+    })
+
+    it('should return 204 on add survey without access token', async () => {
+      const res = await accountCollection.insertOne({
+        name: 'John Doe',
+        email: 'johndoe@mail.com',
+        password: '123456',
+        role: 'admin',
+      })
+
+      const id = res.insertedId
+      const accessToken = sign({ id }, env.jwtSecret)
+
+      await accountCollection.updateOne(
+        {
+          _id: id,
+        },
+        {
+          $set: {
+            accessToken,
+          },
+        }
+      )
+
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'Question',
+          answers: [
+            {
+              image: 'http://image-name.com',
+              answer: 'Answer 1',
+            },
+            {
+              answer: 'Answer 2',
+            },
+          ],
+        })
+        .expect(204)
     })
   })
 })
