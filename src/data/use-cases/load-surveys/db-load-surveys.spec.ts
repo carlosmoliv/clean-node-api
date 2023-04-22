f7e424eed1357a94660ab9065843dd233c8bc6d2
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-04-22 10:15:44 -0300

    test: ensure DbLoadSurveys throws if LoadSurveysRepository throws

--- src/data/use-cases/load-surveys/db-load-surveys.spec.ts
+++ src/data/use-cases/load-surveys/db-load-surveys.spec.ts
@@ -79,4 +79,16 @@ describe('DbLoadSurveys', () => {
 
     expect(httpRespose).toEqual(makeFakeSurveys())
   })
+
+  it('should throw if LoadSurveysRepository throws', async () => {
+    const { sut, loadSurveysRepositoryStub } = makeSut()
+
+    jest
+      .spyOn(loadSurveysRepositoryStub, 'loadAll')
+      .mockReturnValueOnce(Promise.reject(new Error()))
+
+    const promise = sut.load()
+
+    expect(promise).rejects.toThrow()
+  })
 })
