7b0feed70963e15b09f20d8353c89a3175576802
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-04-28 16:45:12 -0300

    test: ensure SurveyMongoRepository find survey by id

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -49,52 +49,23 @@ describe('Survey Mongo Repository', () => {
     })
   })
 
-  describe('loadAll()', () => {
-    it('should load all surveys on success', async () => {
+  describe('loadById()', () => {
+    it('should load survey by id on success', async () => {
       const sut = makeSut()
 
-      await surveyCollection.insertMany([
-        {
-          question: 'any_question',
-          answers: [
-            {
-              image: 'any_image',
-              answer: 'any_answer',
-            },
-            {
-              answer: 'other_answer',
-            },
-          ],
-          date: new Date(),
-        },
-        {
-          question: 'other_question',
-          answers: [
-            {
-              image: 'other_image',
-              answer: 'other_answer',
-            },
-            {
-              answer: 'other_answer',
-            },
-          ],
-          date: new Date(),
-        },
-      ])
+      const res = await surveyCollection.insertOne({
+        question: 'any_question',
+        answers: [
+          {
+            image: 'any_image',
+            answer: 'any_answer',
+          },
+        ],
+        date: new Date(),
+      })
 
-      const surveys = await sut.loadAll()
-
-      expect(surveys.length).toBe(2)
-      expect(surveys[0].question).toBe('any_question')
-      expect(surveys[1].question).toBe('other_question')
-    })
-
-    it('should load an empty list', async () => {
-      const sut = makeSut()
-
-      const surveys = await sut.loadAll()
-
-      expect(surveys.length).toBe(0)
+      const survey = await sut.loadById(res.insertedId.toString())
+      expect(survey).toBeTruthy()
     })
   })
 })
