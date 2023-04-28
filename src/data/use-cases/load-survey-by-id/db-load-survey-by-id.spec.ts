64fdbf030b2a73a277f98d4e5b1f3f40ddd6bee5
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-04-27 23:40:31 -0300

    test: ensure DbLoadSUrveyById return the Survey if LoadSurveyByIdRepository succeeds

---
+++ src/data/use-cases/load-survey-by-id/db-load-survey-by-id.spec.ts
@@ -0,0 +1,69 @@
+import { LoadSurveyByIdRepository } from '@/data/protocols/db/survey/load-survey-by-id-repository'
+import { SurveyModel } from '../../../domain/models/survey'
+import MockDate from 'mockdate'
+import { DbLoadSurveyById } from './db-load-survey-by-id'
+
+const makeFakeSurvey = (): SurveyModel => ({
+  id: 'any_id',
+  question: 'any_question',
+  answers: [
+    {
+      image: 'any_image',
+      answer: 'any_answer',
+    },
+  ],
+  date: new Date(),
+})
+
+const makeLoadSurveyByIdRepositoryStub = (): LoadSurveyByIdRepository => {
+  class LoadSurveyByIdRepositoryStub implements LoadSurveyByIdRepository {
+    async loadById(id: string): Promise<SurveyModel> {
+      return Promise.resolve(makeFakeSurvey())
+    }
+  }
+
+  return new LoadSurveyByIdRepositoryStub()
+}
+
+type SutTypes = {
+  sut: DbLoadSurveyById
+  loadSurveyByIdRepositoryStub: LoadSurveyByIdRepository
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyByIdRepositoryStub = makeLoadSurveyByIdRepositoryStub()
+  const sut = new DbLoadSurveyById(loadSurveyByIdRepositoryStub)
+
+  return {
+    sut,
+    loadSurveyByIdRepositoryStub,
+  }
+}
+
+describe('DbLoadSurveys', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
+  it('should call LoadSurveyByIdRepository', async () => {
+    const { sut, loadSurveyByIdRepositoryStub } = makeSut()
+
+    const loadByIdSpy = jest.spyOn(loadSurveyByIdRepositoryStub, 'loadById')
+
+    await sut.loadById('any_id')
+
+    expect(loadByIdSpy).toHaveBeenCalledWith('any_id')
+  })
+
+  it('should return a Survey on success', async () => {
+    const { sut } = makeSut()
+
+    const surveys = await sut.loadById('any_id')
+
+    expect(surveys).toEqual(makeFakeSurvey())
+  })
+})
