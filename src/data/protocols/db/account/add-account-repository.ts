1959529fb6e4d932290ebf237084da955eaa6d0b
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-04-29 14:54:55 -0300

    feat: ensure save survey result route returns 200 on success

--- src/data/protocols/db/account/add-account-repository.ts
+++ src/data/protocols/db/account/add-account-repository.ts
@@ -1,5 +1,5 @@
-import { AddAccountModel } from '@/domain/use-cases/add-account'
 import { AccountModel } from '@/domain/models/account'
+import { AddAccountModel } from '@/domain/use-cases/account/add-account'
 
 export interface AddAccountRepository {
   add(accountData: AddAccountModel): Promise<AccountModel>
