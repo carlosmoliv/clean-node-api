1978890fa2be33dd4d1a6c333809a99e3b1a58d6
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-04-29 13:33:42 -0300

    feat: ensure that SaveSurveyResultController calls SaveSurveyResult with the correct values

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -4,16 +4,21 @@ import {
   HttpRequest,
   HttpResponse,
   LoadSurveyById,
+  SaveSurveyResult,
 } from './save-survey-result-controller-protocols'
 import { InvalidParamError } from '@/presentation/errors'
 
 export class SaveSurveyResultController implements Controller {
-  constructor(private readonly loadSurveyById: LoadSurveyById) {}
+  constructor(
+    private readonly loadSurveyById: LoadSurveyById,
+    private readonly saveSurveyResult: SaveSurveyResult
+  ) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { surveyId } = httpRequest.params
       const { answer } = httpRequest.body
+      const { accountId } = httpRequest
 
       const survey = await this.loadSurveyById.loadById(surveyId)
 
@@ -26,6 +31,13 @@ export class SaveSurveyResultController implements Controller {
       } else {
         return forbidden(new InvalidParamError('surveyId'))
       }
+
+      await this.saveSurveyResult.save({
+        accountId: accountId,
+        surveyId,
+        answer,
+        date: new Date(),
+      })
 
       return null
     } catch (error) {
