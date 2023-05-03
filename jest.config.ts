144437dab2a137a0a74ab8c3cd9e4e0c1fc6877a
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-05-03 00:17:32 -0300

    chore: exclude mock test folders from coverage and build

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,11 @@ import { Config } from '@jest/types'
 
 const config: Config.InitialOptions = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!**/test/**',
+  ],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   testEnvironment: 'node',
