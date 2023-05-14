de8086081337ff61b36096ae99cd51b02454897b
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-05-14 10:18:07 -0300

    refactor: move sut creation to a factory method

--- src/data/use-cases/survey-result/load-survey-result/db-load-survey-result.spec.ts
+++ src/data/use-cases/survey-result/load-survey-result/db-load-survey-result.spec.ts
@@ -3,17 +3,34 @@ import { SurveyResultModel } from '../save-survey-result/db-save-survey-result-p
 import { LoadSurveyResultRepository } from '@/data/protocols/db/survey-result/load-survey-result-repository'
 import { DbLoadSurveyResult } from './db-load-survey-result'
 
+const mockLoadSUrveyResultRepository = (): LoadSurveyResultRepository => {
+  class LoadSurveyResultRepositoryStub implements LoadSurveyResultRepository {
+    async loadBySurveyId(surveyId: string): Promise<SurveyResultModel> {
+      return new Promise((resolve) => resolve(mockSurveyResultModel()))
+    }
+  }
+
+  return new LoadSurveyResultRepositoryStub()
+}
+
+type SutTypes = {
+  sut: DbLoadSurveyResult
+  loadSurveyResultRepositoryStub: LoadSurveyResultRepository
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyResultRepositoryStub = mockLoadSUrveyResultRepository()
+  const sut = new DbLoadSurveyResult(loadSurveyResultRepositoryStub)
+
+  return {
+    sut,
+    loadSurveyResultRepositoryStub,
+  }
+}
+
 describe('DbLoadSurveyResultUseCase', () => {
   it('should call LoadSurveyResultRepository with correct values', async () => {
-    class LoadSurveyResultRepositoryStub implements LoadSurveyResultRepository {
-      async loadBySurveyId(surveyId: string): Promise<SurveyResultModel> {
-        return new Promise((resolve) => resolve(mockSurveyResultModel()))
-      }
-    }
-
-    const loadSurveyResultRepositoryStub = new LoadSurveyResultRepositoryStub()
-
-    const sut = new DbLoadSurveyResult(loadSurveyResultRepositoryStub)
+    const { sut, loadSurveyResultRepositoryStub } = makeSut()
 
     const loadBySurveyIdSpy = jest.spyOn(
       loadSurveyResultRepositoryStub,
